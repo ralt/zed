95c805b7bad8a5c9a94f46e138811cfe45431bfe
Florian MARGAINE <florian@margaine.com>
2016-01-05 00:20:20 +0100

    Simpler way to return a string

--- run.lisp
+++ run.lisp
@@ -2,21 +2,13 @@
 
 (defun run (command &rest args)
   (handler-case
-      (let ((output (with-output-to-string (out)
-                      (uiop:run-program (apply #'format nil command args)
-                                        :output out)
-                      out)))
-        ;; Most commands return a superfluous newline, remove it
-        (subseq output 0 (1- (length output))))
+      (uiop:run-program (apply #'format nil command args)
+                        :output '(:string :stripped t))
     (uiop:subprocess-error () "")))
 
 (defun run-with-input (input command &rest args)
   (handler-case
-      (let ((output (with-output-to-string (out)
-                      (uiop:run-program (apply #'format nil command args)
-                                        :output out
-                                        :input (make-string-input-stream input))
-                      out)))
-        ;; Most commands return a superfluous newline, remove it
-        (subseq output 0 (1- (length output))))
-    (uiop:subprocess-error () nil)))
+      (uiop:run-program (apply #'format nil command args)
+                        :output '(:string :stripped t)
+                        :input (make-string-input-stream input))
+    (uiop:subprocess-error () "")))
