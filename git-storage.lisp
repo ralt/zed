3b46e6ddbb8f3b68464a741ae72999ecf0c7d3cc
Florian MARGAINE <florian@margaine.com>
2016-01-06 11:46:53 +0100

    Better way to save and print

--- git-storage.lisp
+++ git-storage.lisp
@@ -9,26 +9,9 @@
 (deftype git-tree-type ()
   '(member tree blob))
 
-(defclass git-tree-file ()
-  ((hash :initarg :hash :type string :reader hash)
-   (name :initarg :name :type string :reader name)
-   (type :initarg :type :type git-tree-type :reader tree-type)))
-
 (defclass git-tree (git-object)
-  ((files :initarg :files :type (vector git-tree-file))))
-
-(defun files (tree)
-  (let ((files-list nil))
-    (loop for file across (slot-value tree 'files)
-       do (progn
-            (with-accessors ((tree-type tree-type)
-                             (hash hash) (name name)) file
-              (push (if (eq tree-type 'blob)
-                        "100644" "040000") files-list)
-              (push (string-downcase (symbol-name tree-type)) files-list)
-              (push hash files-list)
-              (push name files-list))))
-    (reverse files-list)))
+  ((blobs :initarg :blobs :accessor blobs :type (vector git-blob))
+   (trees :initarg :trees :accessor trees :type (vector git-tree))))
 
 (defgeneric save (object)
   (:documentation "Saves an object in git database."))
@@ -37,12 +20,31 @@
   (error "Not implemented"))
 
 (defmethod save ((tree git-tree))
-  (setf (hash tree) (run-with-input (format-tree tree) "git mktree")))
+  (loop for tree across (trees tree)
+     do (save tree))
+  (loop for blob across (blobs tree)
+     do (save blob))
+  (setf (hash tree) (run-with-input (print tree) "git mktree")))
 
 (defmethod save ((blob git-blob))
   (setf (hash blob) (run-with-input (content blob) "git hash-object -w --stdin")))
 
-(defun format-tree (tree)
-  (format nil
-          "~{~A ~A ~A~T~A~^~%~}"
-          (files tree)))
+(defmethod print-object ((tree git-tree) stream)
+  (format stream
+          "~{~A~%~}"
+          (loop for blob in (blobs tree)
+             collect (print blob)))
+  (format stream
+          "~{~A~^~%~}"
+          (loop for tree in (trees tree)
+             collect (print-in-tree tree stream))))
+
+(defun print-in-tree (tree stream)
+  (format stream "~A ~A ~A~T~A"
+          "040000" "tree"
+          (hash tree) (hash tree)))
+
+(defmethod print-object ((blob git-blob) stream)
+  (format stream "~A ~A ~A~T~A"
+          "100644" "blob"
+          (hash blob) (hash blob)))
